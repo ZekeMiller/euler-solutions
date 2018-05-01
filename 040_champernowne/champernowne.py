1ac91daa112819a825787427075dda163e5f718b
Zeke Miller <zfm6866@rit.edu>
2018-05-01 19:46:34 -0400

    completed problem 40, started working on a mathematical way to do it, but I'm going to come back to that

---
+++ 040_champernowne/champernowne.py
@@ -0,0 +1,27 @@
+
+
+def champernowne( goalIndices ):
+    goalIndices.sort(reverse = True)
+    upper = goalIndices[0]
+    product = 1
+    digits = 0
+    nextGoal = goalIndices.pop()
+    for i in range( 1, upper ):
+        if digits > upper:
+            break
+        if digits + len(str(i)) >= nextGoal:
+            print( digits, i, nextGoal )
+            product *= int(str(i)[ nextGoal - digits - 1 ] )
+            nextGoal = goalIndices.pop()
+            if len( goalIndices ) == 0:
+                break
+        digits += len(str(i))
+
+    return product
+
+
+def main():
+    print( champernowne( [10**i for i in range( 8 ) ] ) )
+
+
+main()
