bb5fc981edcdedddc4f506fdf295a146e9ad5bf3
Zeke Miller <zfm6866@rit.edu>
2018-05-02 22:58:18 -0400

    solved problem 50.  It's a tad slow, but I have no clue how I would speed it up

---
+++ 050_consecutive_prime_sum/consecutive_prime_sum.py
@@ -0,0 +1,31 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from util import *
+
+
+def findMaxConsecutive( upper ):
+
+    primes = genPrimes( upper )
+    # print( primes )
+    primeSet = set( primes )
+    maxSpan = 0
+    maximum = 0
+
+    for i in range( len( primes ) ):
+        for i2 in range( i + maxSpan, len( primes ) ):
+            sumPrime = sum( primes[i : i + i2] )
+            if sumPrime > upper:
+                break
+            if sumPrime in primeSet:
+                maxSpan = i2
+                maximum = sumPrime
+    return maxSpan, maximum
+
+
+def main():
+    n = 1000000
+    print( findMaxConsecutive( n ) )
+
+
+main()
